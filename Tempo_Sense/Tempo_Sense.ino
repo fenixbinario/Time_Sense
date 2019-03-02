8d9d82de5933c11042fd9dc08669700d3c211c58
Fenix <fbamperius@gmail.com>
2019-03-02 15:57:17 +0100

    Agregar archivos de proyecto.

---
+++ Tempo_Sense/Tempo_Sense.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Tempo_Sense.ino
+ Created:	02/03/2019 15:57:04
+ Author:	jesus
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
